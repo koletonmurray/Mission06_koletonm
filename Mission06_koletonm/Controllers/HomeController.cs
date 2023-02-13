63fe89a658d272b166f104611240c86d91ddb069
Koleton Murray <koletonm@byu.edu>
2023-02-13 09:22:09 -0700

    Add project files.

---
+++ Mission06_koletonm/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_koletonm.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_koletonm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(AddMovieResponse amr)
+        {
+            return View("AddMovie");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_koletonm/Models/AddMovieResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_koletonm.Models
+{
+    public class AddMovieResponse
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [Range(0, 3000)]
+        public ushort Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [MaxLength(25)]
+        public string Notes { get; set; }
+
+    }
+}
